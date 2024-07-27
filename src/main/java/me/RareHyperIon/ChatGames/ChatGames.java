28187e1a6b73f6b93e35c00fe8c7de8efd70de64
RareHyperIon <rarehyperionyt@gmail.com>
2024-07-27 17:15:35 +0100

    Initial Commit

---
+++ src/main/java/me/RareHyperIon/ChatGames/ChatGames.java
@@ -0,0 +1,17 @@
+package me.RareHyperIon.ChatGames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatGames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
